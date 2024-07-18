4a24070394086a0f9b03f8e43ea48a7868e90810
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-07-18 19:26:08 -0400

    Finished get_best_match for responses

---
+++ src/responses.py
@@ -0,0 +1,16 @@
+from difflib import get_close_matches
+import json
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+def get_best_match(user_question: str, questions: dict) -> str | None:
+    questions: list[str] = [q for q in questions]
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+
+    if matches:
+        return matches[0]
